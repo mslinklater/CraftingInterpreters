4224e89836a9157b86cd040a40c811d37a508127
Martin Linklater <mslinklater@gmail.com>
2023-05-19 07:03:55 +0100

    Scanner done

--- Lox/src/com/craftinginterpreters/lox/Lox.java
+++ Lox/src/com/craftinginterpreters/lox/Lox.java
@@ -8,7 +8,8 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.List;
 
-public class Lox {
+public class Lox 
+{
 	static boolean hadError = false;
 	
 	public static void main(String[] args) throws IOException
